317a6d445762e76438588d4e069241a7355db707
Albini Stefano <stefano.albini@epfl.ch>
2026-08-11 17:48:12 +0200

    finished selfAttention layer

--- transformer_layers/selfattention.cc
+++ transformer_layers/selfattention.cc
@@ -51,8 +51,8 @@ void SingleHeadSelfAttn::compute(std::size_t seq_len, uint32_t *input, uint32_t 
     // printf("outPtr = %p\n", (void*)query_layer_out);
     // printf("Query compute() with:\n- seq len = \t %ld\n", seq_len);
     query_layer->compute(seq_len, input, query_layer_out);
-    printf("\n==========================\n");
-    printf("QUERY LAYER OUT:\n");
+    // printf("\n==========================\n");
+    // printf("QUERY LAYER OUT:\n");
     int8_t *res_p_q = (int8_t*)query_layer_out; 
 
 
@@ -67,56 +67,22 @@ void SingleHeadSelfAttn::compute(std::size_t seq_len, uint32_t *input, uint32_t 
     // }
     // exit(0);
 
-    printf("Key\n");
+    // printf("Key\n");
     key_layer->compute(seq_len, input, key_layer_out);
     int8_t *res_p_k = (int8_t*)key_layer_out; 
-    printf("\n==========================\n");
-    printf("KEY LAYER OUT:\n");
+    // printf("\n==========================\n");
+    // printf("KEY LAYER OUT:\n");
 
 
-    // for(int i=0; i<(D_SEQ * (D_Q / 4)); i++){
-    //     printf("[%d]\t", i);
-    //     std::cout << key_layer_out[i] << " --> ";
-    //     int8_t *res_p_q = (int8_t*)&key_layer_out[i]; 
-    //     for(int j=0; j<4; j++){
-    //         printf("%d, ", res_p_q[j]);
-    //     }
-    //     printf("\n");
-    // }
-    // printf("\n");
-
-    // exit(0);
 
     // printf("Value\n");
-    // value_layer->compute(seq_len, input, value_layer_out);
+    value_layer->compute(seq_len, input, value_layer_out);
     // printf("\n==========================\n");
     // printf("VALUE LAYER OUT:\n");
 
-    // for(int i=0; i<(D_SEQ * (D_Q / 4)); i++){
-    //     printf("[%d]\t", i);
-    //     std::cout << value_layer_out[i] << " --> ";
-    //     int8_t *res_p_q = (int8_t*)&value_layer_out[i]; 
-    //     for(int j=0; j<4; j++){
-    //         printf("%d, ", res_p_q[j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    // exit(0);
 
 
 #ifdef BWMA
-
-    // printf("Key layer out:\n");
-    // for(int i=0; i<(pre_seq_len_ * head_hidden_size_ >> 2); i++){
-    //     printf("[%d]\t", i);
-    //     std::cout << key_layer_out[i] << " --> ";
-    //     int8_t *res_p_q = (int8_t*)&key_layer_out[i]; 
-    //     for(int j=0; j<4; j++){
-    //         printf("%d, ", res_p_q[j]);
-    //     }
-    //     printf("\n");
-    // }
 
     std::cout << "BWMA method" << std::endl;
 
@@ -130,82 +96,41 @@ void SingleHeadSelfAttn::compute(std::size_t seq_len, uint32_t *input, uint32_t 
     #endif
 
 
-    // printf("\nKey TRANSPOSED layer out:\n");
-    // for(int i=0; i<(pre_seq_len_ * head_hidden_size_ >> 2); i++){
-    //     printf("[%d]\t", i);
-    //     std::cout << key_transposed_layer_out[i] << std::endl;
-    // }
-
-
-
-#ifdef SIMD
+    #ifdef SIMD
     simdComputeBWMA(seq_len, query_layer_out, attention_scores, key_transposed_layer_out,
                           head_hidden_size_, seq_len);
-#else
+    #else
 
-    #ifdef NANO_3D
+        #ifdef NANO_3D
         smmComputeBWMA_3DLayers(seq_len, query_layer_out, attention_scores, key_transposed_layer_out, head_hidden_size_,
                         seq_len);
-    #else
+        #else
         smmComputeBWMA(seq_len, query_layer_out, attention_scores, key_transposed_layer_out, head_hidden_size_,
                     seq_len);
-    #endif
+        #endif
     
-
-
-    // printf("QK \n");
-    // int8_t *res_qk = (int8_t*)attention_scores; 
-    // printf("\n==========================\n");
-    // printf("QK \n LAYER OUT:\n");
-
-
-    // for(int i=0; i<(pre_seq_len_ * (pre_seq_len_ / 4)); i++){
-    //     printf("[%d]\t", i);
-    //     std::cout << attention_scores[i] << " --> ";
-    //     int8_t *res_qk = (int8_t*)&attention_scores[i]; 
-    //     for(int j=0; j<4; j++){
-    //         printf("%d, ", res_qk[j]);
-    //     }
-    //     printf("\n");
-    // }
-    // printf("\n");
-
-
-    // exit(0);
-
-#endif // SIMD
+    #endif // SIMD
 
 
 
-    #ifdef NANO_3D
+        #ifdef NANO_3D
         softmax->computeRearranged_3Dnano(attention_scores, seq_len, SA_H, SA_W);
-    #else
+        #else
         softmax->computeRearranged(attention_scores, seq_len, kernel_size_);
-    #endif
-    
+        #endif
 
-    printf("\n SOFTMAX res\n");
-    printf("%d x %d\n", pre_seq_len_, pre_seq_len_ >> 2);
-    printf("==============\n");
-    for(int i=0; i<(pre_seq_len_ * (pre_seq_len_ / 4)); i++){
-        printf("[%d]\t", i);
-        std::cout << attention_scores[i] << " --> ";
-        int8_t *res_qk = (int8_t*)&attention_scores[i]; 
-        for(int j=0; j<4; j++){
-            printf("%d, ", res_qk[j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-
-    exit(0);
-
-#ifdef SIMD
+    #ifdef SIMD
     simdComputeBWMA(seq_len, attention_scores, output, value_layer_out, seq_len, head_hidden_size_);
-#else
-    smmComputeBWMA(seq_len, attention_scores, output, value_layer_out, seq_len, head_hidden_size_);
-#endif // SIMD
+    #else
+
+        #ifdef NANO_3D
+        smmComputeBWMA_3DLayers(seq_len, attention_scores, output, value_layer_out, seq_len, head_hidden_size_);
+        #else
+        smmComputeBWMA(seq_len, attention_scores, output, value_layer_out, seq_len, head_hidden_size_);
+        #endif
+
+
+    #endif // SIMD
 
 
 
@@ -216,26 +141,26 @@ void SingleHeadSelfAttn::compute(std::size_t seq_len, uint32_t *input, uint32_t 
                                     pre_seq_len_);
 
 
-                                    #ifdef SIMD
+    #ifdef SIMD
     simdComputeRWMA(seq_len, query_layer_out, attention_scores, key_transposed_layer_out,
                head_hidden_size_, seq_len);
-#else
+    #else
     smmComputeRWMA(seq_len, query_layer_out, attention_scores, key_transposed_layer_out,
                 head_hidden_size_, seq_len);
-#endif // SIMD
+    #endif // SIMD
 
 
     softmax->compute(attention_scores, seq_len);
 
 
     
-#ifdef SIMD
+    #ifdef SIMD
     simdComputeRWMA(seq_len, attention_scores, output, value_layer_out,
                seq_len, head_hidden_size_);
-#else
+    #else
     smmComputeRWMA(seq_len, attention_scores, output, value_layer_out,
                 seq_len, head_hidden_size_);
-#endif // SIMD
+    #endif // SIMD
 
 
 #endif // BWMA
