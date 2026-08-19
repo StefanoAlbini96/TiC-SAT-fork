ae4d4d0fecfd782b8a11a925f168c78efe0b0e2c
Albini Stefano <stefano.albini@epfl.ch>
2026-08-19 12:28:38 +0200

    uncommented the data load/generation

--- transformer.cpp
+++ transformer.cpp
@@ -135,34 +135,34 @@ void test() {
     std::string dir_name = "./weights_data";
 
     uint32_t *tensor_in = new uint32_t[D_SEQ * D_MODEL >> 2];
-// #ifdef RELOAD_WEIGHT
-//     // Load the tensor input from file
-//     // We assign -1 and -1 to n_head and qkv to indicate that we are not loading the weight
-//     loadWeight(-1, -1, D_SEQ * D_MODEL >> 2, tensor_in, dir_name);
-// #else
-//     fill_kernel(tensor_in, D_SEQ * D_MODEL >> 2);
-//     // Save the tensor input to file
-//     // We assign -1 and -1 to n_head and qkv to indicate that we are not saving the weight
-//     saveWeight(-1, -1, D_SEQ * D_MODEL >> 2, tensor_in, dir_name);
-// #endif
+#ifdef RELOAD_WEIGHT
+    // Load the tensor input from file
+    // We assign -1 and -1 to n_head and qkv to indicate that we are not loading the weight
+    loadWeight(-1, -1, D_SEQ * D_MODEL >> 2, tensor_in, dir_name);
+#else
+    fill_kernel(tensor_in, D_SEQ * D_MODEL >> 2);
+    // Save the tensor input to file
+    // We assign -1 and -1 to n_head and qkv to indicate that we are not saving the weight
+    saveWeight(-1, -1, D_SEQ * D_MODEL >> 2, tensor_in, dir_name);
+#endif
 
 
 #ifndef BWMA
-    // uint32_t tensorInRowWise[D_SEQ * D_MODEL >> 2];
-    // // By default, the saved tensor is in block-wise format
-    // // We need to convert it to row-wise format
-    // blockWise2RowWise(tensor_in, tensorInRowWise, D_SEQ, D_MODEL >> 2);
-    // tensor_in = tensorInRowWise;
+    uint32_t tensorInRowWise[D_SEQ * D_MODEL >> 2];
+    // By default, the saved tensor is in block-wise format
+    // We need to convert it to row-wise format
+    blockWise2RowWise(tensor_in, tensorInRowWise, D_SEQ, D_MODEL >> 2);
+    tensor_in = tensorInRowWise;
 
 #else
-    // #ifdef NANO_3D
-    //     // printf("\n\n Rearrangeing INPUTS....\n");
-    //     uint32_t tensorIn3Dnano[D_SEQ * D_MODEL >> 2];
-    //     blockWise2Block3Dnano_inputs(tensor_in, tensorIn3Dnano, D_SEQ, D_MODEL >> 2);
-    //     tensor_in = tensorIn3Dnano;
-    //     // printf("Rearrangeing COMPLETED....\n");
+    #ifdef NANO_3D
+        // printf("\n\n Rearrangeing INPUTS....\n");
+        uint32_t tensorIn3Dnano[D_SEQ * D_MODEL >> 2];
+        blockWise2Block3Dnano_inputs(tensor_in, tensorIn3Dnano, D_SEQ, D_MODEL >> 2);
+        tensor_in = tensorIn3Dnano;
+        // printf("Rearrangeing COMPLETED....\n");
 
-    // #endif
+    #endif
 #endif
 
     uint32_t *out = new uint32_t[D_SEQ * D_MODEL >> 2]();
@@ -175,65 +175,65 @@ void test() {
         volatile auto key_kernel = new uint32_t[D_Q * D_MODEL >> 2]();
         volatile auto value_kernel = new uint32_t[D_Q * D_MODEL >> 2]();
 
-// #ifdef RELOAD_WEIGHT
-//         loadWeight(n, 0, head_qkv_size, query_kernel, dir_name);
-//         loadWeight(n, 1, head_qkv_size, key_kernel, dir_name);
-//         loadWeight(n, 2, head_qkv_size, value_kernel, dir_name);
-// #else
+#ifdef RELOAD_WEIGHT
+        loadWeight(n, 0, head_qkv_size, query_kernel, dir_name);
+        loadWeight(n, 1, head_qkv_size, key_kernel, dir_name);
+        loadWeight(n, 2, head_qkv_size, value_kernel, dir_name);
+#else
 
 
-//         int cnt = 0;
-//         fill_weight(query_kernel, D_MODEL, D_Q >> 2);
-//         fill_weight(key_kernel, D_MODEL, D_Q >> 2);
-//         fill_weight(value_kernel,  D_MODEL, D_Q >> 2);
+        int cnt = 0;
+        fill_weight(query_kernel, D_MODEL, D_Q >> 2);
+        fill_weight(key_kernel, D_MODEL, D_Q >> 2);
+        fill_weight(value_kernel,  D_MODEL, D_Q >> 2);
 
-//         saveWeight(n, 0, head_qkv_size, query_kernel, dir_name);
-//         saveWeight(n, 1, head_qkv_size, key_kernel, dir_name);
-//         saveWeight(n, 2, head_qkv_size, value_kernel, dir_name);
-// #endif
+        saveWeight(n, 0, head_qkv_size, query_kernel, dir_name);
+        saveWeight(n, 1, head_qkv_size, key_kernel, dir_name);
+        saveWeight(n, 2, head_qkv_size, value_kernel, dir_name);
+#endif
 
 
 #ifndef BWMA
-        // // By default, the saved weights are in block-wise format
-        // // We need to convert them to row-wise format
-        // uint32_t* queryRowWise = new uint32_t [D_MODEL * D_Q >> 2];
-        // blockWise2RowWise(query_kernel, queryRowWise, D_MODEL, D_Q >> 2);
-        // query_kernel = queryRowWise;
-        // uint32_t* keyRowWise = new uint32_t [D_MODEL * D_Q >> 2];
-        // blockWise2RowWise(key_kernel, keyRowWise, D_MODEL, D_Q >> 2);
-        // key_kernel = keyRowWise;
-        // uint32_t* valueRowWise = new uint32_t [D_MODEL * D_Q >> 2];
-        // blockWise2RowWise(value_kernel, valueRowWise, D_MODEL, D_Q >> 2);
-        // value_kernel = valueRowWise;
+        // By default, the saved weights are in block-wise format
+        // We need to convert them to row-wise format
+        uint32_t* queryRowWise = new uint32_t [D_MODEL * D_Q >> 2];
+        blockWise2RowWise(query_kernel, queryRowWise, D_MODEL, D_Q >> 2);
+        query_kernel = queryRowWise;
+        uint32_t* keyRowWise = new uint32_t [D_MODEL * D_Q >> 2];
+        blockWise2RowWise(key_kernel, keyRowWise, D_MODEL, D_Q >> 2);
+        key_kernel = keyRowWise;
+        uint32_t* valueRowWise = new uint32_t [D_MODEL * D_Q >> 2];
+        blockWise2RowWise(value_kernel, valueRowWise, D_MODEL, D_Q >> 2);
+        value_kernel = valueRowWise;
 
 #else
 
-    // #ifdef NANO_3D
+    #ifdef NANO_3D
 
-    //     // printf("\n\n Rearrangeing WEIGHTS Q....\n");
-    //     uint32_t* queryBW3Dnano = new uint32_t [D_MODEL * D_Q >> 2];
-    //     // blockWise2Block3Dnano(query_kernel, queryBW3Dnano, D_MODEL, D_Q >> 2);
-    //     blockWise2Block3Dnano_inputs(query_kernel, queryBW3Dnano, D_MODEL, D_Q >> 2);
-    //     query_kernel = queryBW3Dnano;
-    //     // printf("\n\n Rearrangeing WEIGHTS Q DONE....\n");
-
-
-    //     // printf("\n\n Rearrangeing WEIGHTS K....\n");
-    //     uint32_t* keyBW3Dnano = new uint32_t [D_MODEL * D_Q >> 2];
-    //     // blockWise2Block3Dnano(key_kernel, keyBW3Dnano, D_MODEL, D_Q >> 2);
-    //     blockWise2Block3Dnano_inputs(key_kernel, keyBW3Dnano, D_MODEL, D_Q >> 2);
-    //     key_kernel = keyBW3Dnano;
-    //     // printf("\n\n Rearrangeing WEIGHTS K DONE....\n");
+        // printf("\n\n Rearrangeing WEIGHTS Q....\n");
+        uint32_t* queryBW3Dnano = new uint32_t [D_MODEL * D_Q >> 2];
+        // blockWise2Block3Dnano(query_kernel, queryBW3Dnano, D_MODEL, D_Q >> 2);
+        blockWise2Block3Dnano_inputs(query_kernel, queryBW3Dnano, D_MODEL, D_Q >> 2);
+        query_kernel = queryBW3Dnano;
+        // printf("\n\n Rearrangeing WEIGHTS Q DONE....\n");
 
 
-    //     // printf("\n\n Rearrangeing WEIGHTS V....\n");
-    //     uint32_t* valueBW3Dnano = new uint32_t [D_MODEL * D_Q >> 2];
-    //     // blockWise2Block3Dnano(value_kernel, valueBW3Dnano, D_MODEL, D_Q >> 2);
-    //     blockWise2Block3Dnano_inputs(value_kernel, valueBW3Dnano, D_MODEL, D_Q >> 2);
-    //     value_kernel = valueBW3Dnano;
-    //     // printf("\n\n Rearrangeing WEIGHTS V DONE....\n");
+        // printf("\n\n Rearrangeing WEIGHTS K....\n");
+        uint32_t* keyBW3Dnano = new uint32_t [D_MODEL * D_Q >> 2];
+        // blockWise2Block3Dnano(key_kernel, keyBW3Dnano, D_MODEL, D_Q >> 2);
+        blockWise2Block3Dnano_inputs(key_kernel, keyBW3Dnano, D_MODEL, D_Q >> 2);
+        key_kernel = keyBW3Dnano;
+        // printf("\n\n Rearrangeing WEIGHTS K DONE....\n");
 
-    // #endif 
+
+        // printf("\n\n Rearrangeing WEIGHTS V....\n");
+        uint32_t* valueBW3Dnano = new uint32_t [D_MODEL * D_Q >> 2];
+        // blockWise2Block3Dnano(value_kernel, valueBW3Dnano, D_MODEL, D_Q >> 2);
+        blockWise2Block3Dnano_inputs(value_kernel, valueBW3Dnano, D_MODEL, D_Q >> 2);
+        value_kernel = valueBW3Dnano;
+        // printf("\n\n Rearrangeing WEIGHTS V DONE....\n");
+
+    #endif 
     
 #endif
 
@@ -249,56 +249,56 @@ void test() {
     volatile auto ff0_kernel = new uint32_t[D_MODEL * D_FF >> 2]();
     volatile auto ff1_kernel = new uint32_t[D_FF * D_MODEL >> 2]();
 
-// #ifdef RELOAD_WEIGHT
-//     int n = -1; // n=-1 means that we are not saving/loading a head
+#ifdef RELOAD_WEIGHT
+    int n = -1; // n=-1 means that we are not saving/loading a head
 
-//     loadWeight(n, 0, NUM_HEAD * D_Q * D_MODEL >> 2, condense_kernel, dir_name);
-//     loadWeight(n, 1, D_MODEL * D_FF >> 2, ff0_kernel, dir_name);
-//     loadWeight(n, 2, D_MODEL * D_FF >> 2, ff1_kernel, dir_name);
-// #else
-//     fill_weight(condense_kernel, D_MODEL, NUM_HEAD * D_Q >> 2);
-//     fill_weight(ff0_kernel, D_MODEL, D_FF >> 2);
-//     fill_weight(ff1_kernel, D_FF, D_MODEL >> 2);
+    loadWeight(n, 0, NUM_HEAD * D_Q * D_MODEL >> 2, condense_kernel, dir_name);
+    loadWeight(n, 1, D_MODEL * D_FF >> 2, ff0_kernel, dir_name);
+    loadWeight(n, 2, D_MODEL * D_FF >> 2, ff1_kernel, dir_name);
+#else
+    fill_weight(condense_kernel, D_MODEL, NUM_HEAD * D_Q >> 2);
+    fill_weight(ff0_kernel, D_MODEL, D_FF >> 2);
+    fill_weight(ff1_kernel, D_FF, D_MODEL >> 2);
 
-//     int n = -1; // n=-1 means that we are not saving/loading a head
-//     saveWeight(n, 0, NUM_HEAD * D_Q * D_MODEL >> 2, condense_kernel, dir_name);
-//     saveWeight(n, 1, D_MODEL* D_FF >> 2, ff0_kernel, dir_name);
-//     saveWeight(n, 2, D_MODEL* D_FF >> 2, ff1_kernel, dir_name);
-// #endif
+    int n = -1; // n=-1 means that we are not saving/loading a head
+    saveWeight(n, 0, NUM_HEAD * D_Q * D_MODEL >> 2, condense_kernel, dir_name);
+    saveWeight(n, 1, D_MODEL* D_FF >> 2, ff0_kernel, dir_name);
+    saveWeight(n, 2, D_MODEL* D_FF >> 2, ff1_kernel, dir_name);
+#endif
 
 #ifndef BWMA
     // By default, the saved weights are in block-wise format
-    // // We need to convert them to row-wise format
-    // uint32_t* condenseRowWise = new uint32_t [NUM_HEAD * D_Q * D_MODEL >> 2];
-    // blockWise2RowWise(condense_kernel, condenseRowWise, NUM_HEAD * D_Q, D_MODEL >> 2);
-    // condense_kernel = condenseRowWise;
-    // uint32_t* ff0RowWise = new uint32_t [D_MODEL * D_FF >> 2];
-    // blockWise2RowWise(ff0_kernel, ff0RowWise, D_MODEL, D_FF >> 2);
-    // ff0_kernel = ff0RowWise;
-    // uint32_t* ff1RowWise = new uint32_t [D_FF * D_MODEL >> 2];
-    // blockWise2RowWise(ff1_kernel, ff1RowWise, D_FF, D_MODEL >> 2);
-    // ff1_kernel = ff1RowWise;
+    // We need to convert them to row-wise format
+    uint32_t* condenseRowWise = new uint32_t [NUM_HEAD * D_Q * D_MODEL >> 2];
+    blockWise2RowWise(condense_kernel, condenseRowWise, NUM_HEAD * D_Q, D_MODEL >> 2);
+    condense_kernel = condenseRowWise;
+    uint32_t* ff0RowWise = new uint32_t [D_MODEL * D_FF >> 2];
+    blockWise2RowWise(ff0_kernel, ff0RowWise, D_MODEL, D_FF >> 2);
+    ff0_kernel = ff0RowWise;
+    uint32_t* ff1RowWise = new uint32_t [D_FF * D_MODEL >> 2];
+    blockWise2RowWise(ff1_kernel, ff1RowWise, D_FF, D_MODEL >> 2);
+    ff1_kernel = ff1RowWise;
 
 #else
 
-    // #ifdef NANO_3D
+    #ifdef NANO_3D
 
-    //     // printf("\n\n Rearrangeing WEIGHTS CONDENSE....\n");
-    //     uint32_t* condenseKernleBW3Dnano = new uint32_t [NUM_HEAD * D_Q * D_MODEL >> 2];
-    //     blockWise2Block3Dnano_inputs(condense_kernel, condenseKernleBW3Dnano, NUM_HEAD * D_Q, D_MODEL >> 2);
-    //     condense_kernel = condenseKernleBW3Dnano;
+        // printf("\n\n Rearrangeing WEIGHTS CONDENSE....\n");
+        uint32_t* condenseKernleBW3Dnano = new uint32_t [NUM_HEAD * D_Q * D_MODEL >> 2];
+        blockWise2Block3Dnano_inputs(condense_kernel, condenseKernleBW3Dnano, NUM_HEAD * D_Q, D_MODEL >> 2);
+        condense_kernel = condenseKernleBW3Dnano;
 
-    //     // printf("\n\n Rearrangeing FF0....\n");
-    //     uint32_t* ff0KernleBW3Dnano = new uint32_t [D_MODEL * D_FF >> 2];
-    //     blockWise2Block3Dnano_inputs(ff0_kernel, ff0KernleBW3Dnano, D_MODEL, D_FF >> 2);
-    //     ff0_kernel = ff0KernleBW3Dnano;
+        // printf("\n\n Rearrangeing FF0....\n");
+        uint32_t* ff0KernleBW3Dnano = new uint32_t [D_MODEL * D_FF >> 2];
+        blockWise2Block3Dnano_inputs(ff0_kernel, ff0KernleBW3Dnano, D_MODEL, D_FF >> 2);
+        ff0_kernel = ff0KernleBW3Dnano;
         
-    //     // printf("\n\n Rearrangeing FF1....\n");
-    //     uint32_t* ff1KernleBW3Dnano = new uint32_t [D_FF * D_MODEL >> 2];
-    //     blockWise2Block3Dnano_inputs(ff1_kernel, ff1KernleBW3Dnano, D_FF, D_MODEL >> 2);
-    //     ff1_kernel = ff1KernleBW3Dnano;
+        // printf("\n\n Rearrangeing FF1....\n");
+        uint32_t* ff1KernleBW3Dnano = new uint32_t [D_FF * D_MODEL >> 2];
+        blockWise2Block3Dnano_inputs(ff1_kernel, ff1KernleBW3Dnano, D_FF, D_MODEL >> 2);
+        ff1_kernel = ff1KernleBW3Dnano;
 
-    // #endif
+    #endif
 
 
 
